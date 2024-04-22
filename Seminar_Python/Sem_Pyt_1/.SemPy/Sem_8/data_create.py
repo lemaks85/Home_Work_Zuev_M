906ce38eb0f5a3a790a4eb2b4c39fc62558bd584
lemaks85 <sxcar@yandex.ru>
2024-04-22 09:21:55 +0300

    Create program final DZ

--- Seminar_Python/Sem_Pyt_1/.SemPy/Sem_8/data_create.py
+++ Seminar_Python/Sem_Pyt_1/.SemPy/Sem_8/data_create.py
@@ -1,6 +1,6 @@
 def input_user_data():
-    name = input("Введите имя: ")
-    surname = input("Введите фамилию: ")
-    phone = input("Введите телефон: ")
-    adress = input("Введите адрес: ")
-    return name, surname, phone, adress
+    name = input("Введите текст заметки: ")
+    #surname = input("Введите фамилию: ")
+    #phone = input("Введите телефон: ")
+    #adress = input("Введите адрес: ")
+    return name, #surname, phone, adress
