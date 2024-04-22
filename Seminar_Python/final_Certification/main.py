c5f155aaf7da8affa856cfec080470c38720cee6
lemaks85 <sxcar@yandex.ru>
2024-04-22 09:45:08 +0300

    Create connect BD  for my programm

--- Seminar_Python/final_Certification/main.py
+++ Seminar_Python/final_Certification/main.py
@@ -3,3 +3,16 @@ from tkinter import ttk
 from tkinter import messagebox
 import sqlite3
 
+connection = sqlite3.connect('note.db')
+cur = connection.cursor()
+
+cur.execute("""
+            CREATE TABLE IF NOT EXISTS transactions (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            type TEXT,
+            amount REAL,
+            comment TEXT
+                )
+""")
+
+connection.commit()
