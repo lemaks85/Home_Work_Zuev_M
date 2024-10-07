184746b22a46b1f1ab253d2f6fc36ea649fc1e78
lemaks85 <sxcar@yandex.ru>
2024-10-07 10:56:10 +0300

    Task_1 complete

---
+++ Seminar_PHP/DZ_2/dz-work.php
@@ -0,0 +1,49 @@
+<?php
+
+// function arithmeticOperation($num1, $num2, $operation)
+// {
+//         if($operation == "+")
+//             return $num1 + $num2;
+//         if($operation == "-")
+//             return $num1 - $num2;
+//         if($operation == "*")
+//             return $num1 * $num2;
+//         if($operation == "/")
+//             return $num1 / $num2;
+//         return "There is no such operator";
+// }
+
+// $num1 = 4;
+// $num2 = 5;
+// $operation = "+";
+
+// echo arithmeticOperation ($num1, $num2, $operation);
+
+// ************************************************************************************************************************************
+
+// function  mathOperation($arg1, $arg2, $operation)
+// {
+//     switch ($operation)
+//     {
+//         case "+":
+//             return $arg1 + $arg2;
+//             break;
+//         case "-":
+//             return $arg1 - $arg2;
+//             break;
+//         case "*":
+//             return $arg1 * $arg2;
+//             break;
+//         case "/":
+//             return $arg1 / $arg2;
+//             break;
+//         default:
+//             return "There is no such operator";
+//     }
+// }
+
+// $arg1 = 4;
+// $arg2 = 5;
+// $operation = "+";
+
+// echo mathOperation($arg1, $arg2, $operation);
