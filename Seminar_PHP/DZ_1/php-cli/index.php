fff6051aa070391fa72dd19d0a03b9895845634f
lemaks85 <sxcar@yandex.ru>
2024-11-11 16:10:27 +0300

    DZ-5 PHP

--- Seminar_PHP/DZ_1/php-cli/index.php
+++ Seminar_PHP/DZ_1/php-cli/index.php
@@ -1,27 +1,3 @@
 <?php
-    $a = 5;
-    $b = '05';
-    echo '<pre>';
-        var_dump($a == $b);
-    echo '</pre>';
-    echo '<pre>';
-        var_dump((int)'012345');
-        echo '</pre>';
-    echo '<pre>';
-        var_dump((float)123.0 != (int)123.0);
-    echo '</pre>';
-    echo '<pre>';
-    var_dump(0 == 'hello, world ');
-    echo '</pre>';
-$a = 1;
-$b = 2;
-
-echo "A = $a <br/>";
-echo "B = $b <br/>";
-$a = $a + 1;
-echo "A = $a <br/>";
-$b = $a - 1;
-echo "B = $b <br/>";
-
 
 phpinfo();
