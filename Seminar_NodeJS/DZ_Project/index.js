f6e53aab8e7ae808a8664bbad03b35b30e1848d4
lemaks85 <sxcar@yandex.ru>
2024-12-05 14:39:09 +0300

    server work

--- Seminar_NodeJS/DZ_Project/index.js
+++ Seminar_NodeJS/DZ_Project/index.js
@@ -3,14 +3,14 @@ const server = http.createServer((req, res) => {
 
     if(req.url === '/') {
         res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
-        res.end(`<h1>Main_page</h1> <a href="/about">To about</a>`);
+        res.end(`<h1>Main page</h1> <a href="/about">To about</a>`);
     } else if (req.url === '/about'){
         res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
-        res.end(`<h1>About_page</h1> <a href="/">To main</a>`);
+        res.end(`<h1>About page</h1> <a href="/">To main</a>`);
     } else {
         res.writeHead(404, {'Content-Type': 'text/html; charset=UTF-8'});
-        res.end(<h1>Page not found 404</h1>);
+        res.end(`<h1>Page not found 404</h1>`);
     }
-})
+});
 
-server.listen = 3000;
+server.listen('3000');
