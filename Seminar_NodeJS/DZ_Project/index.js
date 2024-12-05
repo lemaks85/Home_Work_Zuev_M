395bb0fe6f8d0b02a60eb0017854ce2824a2895a
lemaks85 <sxcar@yandex.ru>
2024-12-05 14:11:35 +0300

    main

--- Seminar_NodeJS/DZ_Project/index.js
+++ Seminar_NodeJS/DZ_Project/index.js
@@ -1,7 +1,14 @@
 const http = require('http');
 const server = http.createServer((req, res) => {
 
-
+    if(req.url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html; charset=UTF-8'});
+        res.end(
+            `<h1>Main_page</h1>
+            <a href="/about">To about</a>'
+        );
+    } else if (req.url === '/about'){
+     }
 
 
 })
