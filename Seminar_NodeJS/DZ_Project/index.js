6d39af0284287a4d5ca3946284f65a51f8c9582e
lemaks85 <sxcar@yandex.ru>
2024-12-06 19:31:43 +0300

    work or dz

--- Seminar_NodeJS/DZ_Project/index.js
+++ Seminar_NodeJS/DZ_Project/index.js
@@ -1,38 +1,59 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
-
+const isToFile = path.join(__dirname, "ticPages.json");
 const app = express();
+const ticPage = [
+        {
+            "page": "/",
+            "tic": 0
+        },
 
+        {
+            "page": "/about",
+            "tic": 0
+        }];
 
+fs.readFile('./ticPages.json', (err, data) => {
+    if (err) {
+        console.error('File not found ! To be create new file')
+        const jsonData = JSON.stringify(ticPage, null, 2);
+        fs.writeFile('ticPages.json', jsonData, (err) => {
+        if (err) {
+            console.error('Error writing file:', err);
+        } else {
+            console.log('JSON data has been written to data.json');
+        }});
+        return
+    }
+        console.log(data.toString());
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const port = 3000;
-app.listen(port, () => {
-    console.log('Server start');
-
+fs.readFile(isToFile, "utf-8", (error, data) => {
+    if (error) return console.log(error);
+        let isToData = JSON.parse(data, "utf-8");
+        isToData[1].tic += 1;
+        console.log(isToData);
+fs.writeFile(isToFile, JSON.stringify(isToData, null, 2), (error) => {
+    if (error) return console.log(error);
+        });
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const port = 3000;
+// app.listen(port, () => {console.log('Server start');});
 
 
 
