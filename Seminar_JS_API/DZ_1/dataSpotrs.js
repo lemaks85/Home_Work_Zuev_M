96b774b4014dd6e0d55ce46170ee7350c0217f67
lemaks85 <sxcar@yandex.ru>
2024-12-16 17:44:50 +0300

    data.js

--- Seminar_JS_API/DZ_1/dataSpotrs.js
+++ Seminar_JS_API/DZ_1/dataSpotrs.js
@@ -1,16 +1,51 @@
 export const programmSports = `[
 {
-"id": "1",
-"name": "Smart Start Fullbody",
-"time": "09:00 30 min",
-"maxPeople": "30",
-"currentPeople": "15"
-}
-
-
-
-
-
-
+    "id": "1",
+    "name": "Smart Start Fullbody",
+    "coach": "DDX Team",
+    "time": "09:00 30 min",
+    "maxPeople": "30",
+    "currentPeople": "15"
+},
+{
+    "id": "2",
+    "name": "DDX ATHLETIC",
+    "coach": "Наталья Завалий",
+    "time": "09:30 50 min",
+    "maxPeople": "30",
+    "currentPeople": "27"
+},
+{
+    "id": "3",
+    "name": "YOGA",
+    "coach": "Ксения Капранова",
+    "time": "10:30 90 min",
+    "maxPeople": "15",
+    "currentPeople": "10"
+},
+{
+    "id": "4",
+    "name": "3D Ягодицы 45",
+    "coach": "DDX Team",
+    "time": "13:00 45 min",
+    "maxPeople": "30",
+    "currentPeople": "15"
+},
+{
+    "id": "5",
+    "name": "ZUMBA 55",
+    "coach": "DDX Team",
+    "time": "17:00 55 min",
+    "maxPeople": "30",
+    "currentPeople": "22"
+},
+{
+    "id": "6",
+    "name": "BODYSTEP 55",
+    "coach": "София Рзаева",
+    "time": "19:00 55 min",
+    "maxPeople": "30",
+    "currentPeople": "29"
+},
 
 ]`
