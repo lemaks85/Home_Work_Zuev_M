faabaa7a5e791c308a4cdf0002b6d06c852739fc
lemaks85 <sxcar@yandex.ru>
2025-06-09 19:36:36 +0300

    HW_3

--- Seminar_LARAVEL/Project/vmas-app/routes/web.php
+++ Seminar_LARAVEL/Project/vmas-app/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FormProcessor;
+use App\Models\Employee;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +10,11 @@ Route::get('/', function () {
 
 Route::get('/userform', [FormProcessor::class, 'index'])->name('user-form');
 Route::post('/store_form',[FormProcessor::class, 'store']);
+
+Route::get('/test_database', function () {
+    $newUser = 'newUser2';
+    $employee = new Employee();
+    $employee->first_name = $newUser;
+    $employee->save();
+     echo "Добавлен пользователь $newUser.";
+    });
