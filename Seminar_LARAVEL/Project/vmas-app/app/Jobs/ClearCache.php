3244ccd390f675219ac506fa3a532e6d6915395d
lemaks85 <sxcar@yandex.ru>
2025-06-10 13:33:28 +0300

    hw-10

---
+++ Seminar_LARAVEL/Project/vmas-app/app/Jobs/ClearCache.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+use Illuminate\Support\Facades\File;
+
+class ClearCache implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        file_put_contents("storage/logs/laravel.log", '');
+    }
+}
--- Seminar_LARAVEL/Project/vmas-app/routes/web.php
+++ Seminar_LARAVEL/Project/vmas-app/routes/web.php
@@ -11,27 +11,44 @@ use App\Http\Controllers\LogController;
 use App\Events\NewsHidden;
 use App\Models\News;
 
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/news/create-test', function () {
-    $news = new News;
-    $news->title = 'Test news title';
-    $news->body = 'Test news body';
 
-    $news->save();
-    return $news;
-});
 
-Route::get('/news/{id}/hide', function ($id) {
-    $news = News::findOrFail($id);
-    $news->hidden = true;
-    $news->save();
 
-    NewsHidden::dispatch($news);
-    return 'News hidden';
-});
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::get('/news/create-test', function () {
+//     $news = new News;
+//     $news->title = 'Test news title';
+//     $news->body = 'Test news body';
+
+//     $news->save();
+//     return $news;
+// });
+
+// Route::get('/news/{id}/hide', function ($id) {
+//     $news = News::findOrFail($id);
+//     $news->hidden = true;
+//     $news->save();
+
+//     NewsHidden::dispatch($news);
+//     return 'News hidden';
+// });
+
+
+
+
+
+
+
+
 // Route::get('/', function () {
 //     return view('welcome');
 // });
